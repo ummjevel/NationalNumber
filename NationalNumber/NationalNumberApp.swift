bc69fe7ce5234136554b7a526c208a657e66f853
ummjevel <mseagle2023@gmail.com>
2021-04-27 22:34:04 +0900

    google places 검색까지 붙임

--- NationalNumber/NationalNumberApp.swift
+++ NationalNumber/NationalNumberApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import GoogleMaps
 import GooglePlaces
 
-let googleApiKey = "AIzaSyDB5k7_v016Zz4nj-DpSdZpv6D-A2-5CKE"
+let googleApiKey = "AIzaSyBKgzQuhGQkNutI21j17ynayLRnQ7iOL7A"
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
